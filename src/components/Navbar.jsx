4f8bc5ec114aa1e38a35539181f1ef9e4efadbc8
Thomas-zz-2626 <19124164774tzz@gmail.com>
2026-07-27 20:12:26 +0800

    Redesign navbar: semi-transparent, no glass, gradient fade bottom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ function Navbar({ scrolled }) {
   return (
     <nav className={`navbar${scrolled ? ' navbar--scrolled' : ''}`}>
       <div className="navbar__inner">
-        <a href="#hero" className="navbar__logo">PORTFOLIO</a>
+        <a href="#hero" className="navbar__logo">CHEN WEI</a>
         <ul className="navbar__links">
-          <li><a href="#about">简介</a></li>
-          <li><a href="#projects">作品</a></li>
-          <li><a href="#contact">联系</a></li>
+          <li><a href="#projects">工作</a></li>
+          <li><a href="#about">关于</a></li>
+          <li><a href="#contact">接触</a></li>
         </ul>
       </div>
     </nav>
